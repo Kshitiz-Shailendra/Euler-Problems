b9b33eb3f558578a25106bc8b60686ec076bda3c
Kshitiz <kshitizsjobs@gmail.com>
2015-09-20 17:53:01 +0530

    done programs

---
+++ EulerProject/src/com/learning/algo/Q29DigitPowers.java
@@ -0,0 +1,33 @@
+package com.learning.algo;
+
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Q29DigitPowers {
+	
+	public int uniquePowers(int limit){
+		Set<BigInteger>  powerSet = new HashSet<BigInteger>();
+		
+		for(int i=2;i<=limit;i++){
+			BigInteger num = new BigInteger(Integer.valueOf(i).toString());
+			for(int j=2;j<=limit;j++){
+				powerSet.add(num.pow(j));
+			}
+		}
+		return powerSet.size();
+	}
+	
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		Q29DigitPowers powers = new Q29DigitPowers();
+		System.out.println(powers.uniquePowers(100));
+	
+		
+		
+		long stop = System.currentTimeMillis();
+		System.out.println("time " + (stop - start));
+
+	}
+
+}
